9ea3cb56cc472c8b4489109ba521fbccb8ae0a0b
Emily <emi.a.v@proton.me>
2026-09-02 20:13:10 +0200

    Added altitude and speed info to attitude display.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"time"
 
@@ -78,20 +79,25 @@ func object(i int) state.FlightData {
 		Capacity:    3000,
 	}
 
+	var randomVal float32
+	if i%3600 == 0 {
+		randomVal = 0.5 - rand.Float32()
+	}
+
 	return state.FlightData{
 		Time:          0,
 		Name:          "J-8 MFD Test",
-		Altitude:      165.151365514,
-		Airspeed:      43.161341234,
+		Altitude:      165.151365514 + float32(i),
+		Airspeed:      43.161341234 + float32(i)*1,
 		Pitch:         5.136514,
 		Roll:          (float32(i%1800) * 0.5),
 		G:             1.1365341,
-		AGL:           0.13561342,
+		AGL:           0.13561342 + float32(i),
 		Heading:       (float32(i%360) * 1),
 		Climb:         12.135123,
 		Mass:          10000.135123,
 		Mach:          0.21353,
-		Alpha:         3.05135343,
+		Alpha:         3.05135343 + (float32(i) * (randomVal / float32(i%3600))),
 		Latitude:      35.604,  //+ float32(math.Sin(stateNavigation.DegToRad((float64(i%360)*1)))*0.1),
 		Longitude:     51.8061, // + float32(math.Cos(stateNavigation.DegToRad((float64(i%360)*1)))*0.1),
 		JetEngines:    []state.JetEngine{eng, eng, eng, eng, eng},
--- internal/stateAttitude/state.go
+++ internal/stateAttitude/state.go
@@ -2,6 +2,7 @@ package stateAttitude
 
 import (
 	"math"
+	"strconv"
 
 	"example.com/MFDTest/internal/state"
 	"example.com/MFDTest/internal/stateNavigation"
@@ -41,20 +42,94 @@ func (s _state) drawHorizon(maxDegree float32) {
 	innnerRadius := state.ScaleF(50)
 	outerRadius := state.ScaleF(150)
 
-	inX := math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * float64(innnerRadius)
-	inY := math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * float64(innnerRadius)
-	outX := math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * float64(outerRadius)
-	outY := math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * float64(outerRadius)
+	sin := math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)))
+	cos := math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)))
+
+	inX := cos * float64(innnerRadius)
+	inY := sin * float64(innnerRadius)
+	outX := cos * float64(outerRadius)
+	outY := sin * float64(outerRadius)
 
 	rl.DrawLine(anchorX-int32(inX), anchorY-int32(inY), anchorX-int32(outX), anchorY-int32(outY), state.COLOR_SELECT)
+	rl.DrawLine(anchorX-int32(outX)-int32(float32(sin)*state.ScaleF(18)), anchorY-int32(outY)+int32(float32(cos)*state.ScaleF(18)), anchorX-int32(outX), anchorY-int32(outY), state.COLOR_SELECT)
+
 	rl.DrawLine(anchorX+int32(inX), anchorY+int32(inY), anchorX+int32(outX), anchorY+int32(outY), state.COLOR_SELECT)
+	rl.DrawLine(anchorX+int32(outX)-int32(float32(sin)*state.ScaleF(18)), anchorY+int32(outY)+int32(float32(cos)*state.ScaleF(18)), anchorX+int32(outX), anchorY+int32(outY), state.COLOR_SELECT)
 
 	// Center
 	rl.DrawRectangle(anchorX-state.Scale(4), anchorY-state.Scale(4), state.Scale(8), state.Scale(8), state.COLOR_SELECT)
 
 	// Glide Slope
 	glideSlopeOffset := float64((-state.GlobalFlightData.Alpha)/maxDegree) * float64(height)
-	glideSlopeOffsetX := float32(math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * glideSlopeOffset)
-	glideSlopeOffsetY := float32(math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * glideSlopeOffset)
+	glideSlopeOffsetX := -float32(math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * glideSlopeOffset)
+	glideSlopeOffsetY := float32(math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll))) * glideSlopeOffset)
 	rl.DrawRing(rl.Vector2{X: float32(anchorX) - glideSlopeOffsetX, Y: float32(anchorY) - glideSlopeOffsetY}, state.ScaleF(8), state.ScaleF(10), 0, 360, 0, state.COLOR_SELECT)
+	rl.DrawLine(
+		anchorX-int32(glideSlopeOffsetX)+int32(cos*float64(state.Scale(10))),
+		anchorY-int32(glideSlopeOffsetY)+int32(sin*float64(state.Scale(10))),
+		anchorX-int32(glideSlopeOffsetX)+int32(cos*float64(state.Scale(24))),
+		anchorY-int32(glideSlopeOffsetY)+int32(sin*float64(state.Scale(24))),
+		state.COLOR_SELECT,
+	)
+	rl.DrawLine(
+		anchorX-int32(glideSlopeOffsetX)-int32(cos*float64(state.Scale(10))),
+		anchorY-int32(glideSlopeOffsetY)-int32(sin*float64(state.Scale(10))),
+		anchorX-int32(glideSlopeOffsetX)-int32(cos*float64(state.Scale(24))),
+		anchorY-int32(glideSlopeOffsetY)-int32(sin*float64(state.Scale(24))),
+		state.COLOR_SELECT,
+	)
+	rl.DrawLine(
+		anchorX-int32(glideSlopeOffsetX)-int32(math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)-90))*float64(state.Scale(10))),
+		anchorY-int32(glideSlopeOffsetY)-int32(math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)-90))*float64(state.Scale(10))),
+		anchorX-int32(glideSlopeOffsetX)-int32(math.Cos(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)-90))*float64(state.Scale(24))),
+		anchorY-int32(glideSlopeOffsetY)-int32(math.Sin(stateNavigation.DegToRad(float64(state.GlobalFlightData.Roll)-90))*float64(state.Scale(24))),
+		state.COLOR_SELECT,
+	)
+
+	// Spd Info
+	strSpd := strconv.FormatInt(int64(math.Round(float64(state.GlobalFlightData.Airspeed))), 10) + "m/s"
+	infoMargin := state.Scale(8)
+	infoOutline := state.Scale(2)
+	infoFontSize := state.Scale(20)
+	infoAnchorX := state.Scale(state.SCREEN_MARGIN*2 + state.MENU_BUTTON_SIZE)
+	infoAnchorY := int32(rl.GetScreenHeight()/2) - infoMargin*2 - infoFontSize/2
+
+	wheelOffset := state.Scale(200)
+	relSpeed := math.Mod(float64(state.GlobalFlightData.Airspeed), 10) / 10
+	var spdWidth int32
+	for i := 0; i < 20; i++ {
+		spdWidth = state.Scale(20)
+		rl.DrawRectangle(
+			state.Scale(state.SCREEN_MARGIN*2+state.MENU_BUTTON_SIZE),
+			(anchorY-wheelOffset)+(int32(i)*state.Scale(20))+int32(math.Round(20*relSpeed)),
+			spdWidth,
+			state.Scale(2),
+			state.COLOR_SELECT,
+		)
+	}
+	rl.DrawRectangle(infoAnchorX+state.Scale(30), infoAnchorY, infoOutline*2+rl.MeasureText(strSpd, infoFontSize)+infoMargin*2, infoMargin*2+infoOutline*2+infoFontSize, state.COLOR_SELECT)
+	rl.DrawRectangle(infoAnchorX+state.Scale(30)+infoOutline, infoAnchorY+infoOutline, infoMargin*2+rl.MeasureText(strSpd, infoFontSize), infoMargin*2+infoFontSize, state.COLOR_UNSELECT)
+	rl.DrawText(strSpd, infoAnchorX+state.Scale(30)+infoMargin+infoOutline, infoAnchorY+infoMargin+infoOutline, infoFontSize, state.COLOR_SELECT)
+
+	// Alt Info
+	strAlt := strconv.FormatInt(int64(math.Round(float64(state.GlobalFlightData.Altitude))), 10) + "m"
+	infoAnchorX = int32(rl.GetRenderWidth()) - state.Scale(state.SCREEN_MARGIN*2+state.MENU_BUTTON_SIZE)
+
+	wheelOffset = state.Scale(200)
+	relAlt := math.Mod(float64(state.GlobalFlightData.Altitude), 10) / 10
+	// var altWidth int32
+	for i := 0; i < 20; i++ {
+		// spdWidth = state.Scale(20)
+		rl.DrawRectangle(
+			int32(rl.GetRenderWidth())-state.Scale(state.SCREEN_MARGIN*2+state.MENU_BUTTON_SIZE)-state.Scale(20),
+			(anchorY-wheelOffset)+(int32(i)*state.Scale(20))+int32(math.Round(20*relAlt)),
+			state.Scale(20),
+			state.Scale(2),
+			state.COLOR_SELECT,
+		)
+	}
+	rl.DrawRectangle(infoAnchorX-state.Scale(50)-rl.MeasureText(strAlt, infoFontSize), infoAnchorY, infoOutline*2+rl.MeasureText(strAlt, infoFontSize)+infoMargin*2, infoMargin*2+infoOutline*2+infoFontSize, state.COLOR_SELECT)
+	rl.DrawRectangle(infoAnchorX-state.Scale(50)-rl.MeasureText(strAlt, infoFontSize)+infoOutline, infoAnchorY+infoOutline, infoMargin*2+rl.MeasureText(strAlt, infoFontSize), infoMargin*2+infoFontSize, state.COLOR_UNSELECT)
+	rl.DrawText(strAlt, infoAnchorX-state.Scale(50)-rl.MeasureText(strAlt, infoFontSize)+infoMargin+infoOutline, infoAnchorY+infoMargin+infoOutline, infoFontSize, state.COLOR_SELECT)
+
 }
